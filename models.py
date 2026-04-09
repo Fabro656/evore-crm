fcc30f0236a9037bf3b1f2d54cd099d56e2dbc48
Fabricio <fabricio@Tatiianas-Mac-mini.local>
2026-04-09 10:31:21 +0300

    fix login_manager import

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 # models.py — All SQLAlchemy models + DB init
-from extensions import db
+from extensions import db, login_manager
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, date as date_type
